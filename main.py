4528271a11db7c301199f5b34d4176da4afe33fc
pnv28 <pnv.khemka@gmail.com>
2023-06-07 19:45:35 +0545

    .

--- main.py
+++ main.py
@@ -6,11 +6,8 @@ import logging
 
 logger = logging.getLogger('discord')
 logger.setLevel(logging.DEBUG)
-handler = logging.FileHandler(filename='discord.log',
-                              encoding='utf-8',
-                              mode='w')
-handler.setFormatter(
-  logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
 logger.addHandler(handler)
 
 bot = discord.Bot()
