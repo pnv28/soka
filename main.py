4285d307fba045fa0db97ba67a72898e13ab9571
pnv28 <72593874+pnv28@users.noreply.github.com>
2023-06-07 13:25:07 +0000

    Adding dotenv

--- main.py
+++ main.py
@@ -2,26 +2,30 @@ import discord
 import os
 import logging
 
+token = os.environ['token']
+
 logger = logging.getLogger('discord')
 logger.setLevel(logging.DEBUG)
-handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
-handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+handler = logging.FileHandler(filename='discord.log',
+                              encoding='utf-8',
+                              mode='w')
+handler.setFormatter(
+  logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
 logger.addHandler(handler)
 
-bot=discord.Bot()
+bot = discord.Bot()
+
 
 @bot.event
 async def on_ready():
-    print(f"{bot.user} is ready and online")
+  print(f"{bot.user} is ready and online")
 
-cogList = [
-    'dev',
-    'moderation'
-]
+
+cogList = ['dev', 'moderation']
 
 print("Loading Cogs")
 for cog in cogList:
-    bot.load_extension(f'cogs.{cog}')
-    print(f"Loaded {cog}.py")
+  bot.load_extension(f'cogs.{cog}')
+  print(f"Loaded {cog}.py")
 
-bot.run("MTExNTY0MTQyMTY1MTc4NzgzNg.GOYjxU.57VoHKzxGu-qjJZRcnyWsb_FDJn3aTkAGPMbSs")
+bot.run(token)
